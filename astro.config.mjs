d612c2f571984ece72719c1a257dc863ce194f16
Joel Daniel Rico <joeldanielrico@gmail.com>
2025-07-19 23:22:42 -0700

    new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,11 @@
-import { defineConfig } from "astro/config";
-
+// @ts-check
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  integrations: [],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  redirects: {
-    "/github": "https://github.com/jjoeldaniel",
-    "/linkedin": "https://linkedin.com/in/joeldanielrico",
-    "/steam": "https://steamcommunity.com/id/jjoeldaniel/",
-    "/resume": "https://github.com/jjoeldaniel/resume/blob/main/resume.pdf"
-  },
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark',
-    },
-  }
+  adapter: cloudflare(),
+  vite: { plugins: [tailwindcss()], },
 });
